22a7912b3d25f20ef3b1956d1da41249ae2122f1
Eduardo Vieira <167880647+Edu936@users.noreply.github.com>
2025-04-14 20:58:37 -0300

    Retomando o estudo rotas

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,11 +8,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent implements OnInit {
+  //injetado a dependecia do serviço ActivadedRoute
   private _router: ActivatedRoute = inject(ActivatedRoute);
+  //criado um atributo para essa pagina 
   public valor = 0;
-
   ngOnInit(): void {
+    //capturando o valor presente na url
     this.valor = parseInt(this._router.snapshot.params['id'],10);
+    //exibindo o valor no terminal  
     console.log(this.valor)
   }
 }
