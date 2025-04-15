0000b18ca55f90307fc3f25862a5958a57073af0
Eduardo Vieira <167880647+Edu936@users.noreply.github.com>
2025-04-14 23:49:35 -0300

    configurando titulos dinamicos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,41 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { PersonComponent } from './components/person/person.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const resolvedTitle: ResolveFn<string> = () => Promise.resolve('Oi');
 
 export const routes: Routes = [
     {
         path: 'home',
+        title: 'Pagina Inicial',
         component: HomeComponent,
     },
     {
         path: 'about',
+        title: resolvedTitle,
         component: AboutComponent,
     },
     {
         path: 'profile/:id',
+        title: 'Perfil',
         component: ProfileComponent
     },
     {
         path: 'person/:personId',
+        title: 'Pessoa',
         component: PersonComponent,
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+    },
+    {
+        path: '**',
+        title: '404',
+        component: PageNotFoundComponent
     }
 ];
