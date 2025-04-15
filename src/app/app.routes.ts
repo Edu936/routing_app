1b26b0a5f4f59d28dfb6e6fb5080233a0e72466e
Eduardo Vieira <167880647+Edu936@users.noreply.github.com>
2025-04-14 21:03:17 -0300

    Primeiras rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [
-    {
-        path: 'home',
-        component: HomeComponent,
-    },
-    {
-        path: 'about',
-        component: AboutComponent,
-    },
-    {
-        path: "profile/:id",
-        component: ProfileComponent,
-    }
-];
+export const routes: Routes = [];
