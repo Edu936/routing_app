49184a2079329ec05e7b7f45719f0ac4c0e6c104
Eduardo Vieira <167880647+Edu936@users.noreply.github.com>
2025-04-15 22:35:54 -0300

    Utilizando router

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  name = 'teste';
+  private readonly _router = inject(Router);
+  teste() {
+    this._router.navigate(['person', {name}]);
+  }
 }
